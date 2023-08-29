b7b9d7f196af06d86109825c58e62527958a53fe
Abdelalim <abdelalim.dev@gmail.com>
2023-08-29 10:20:12 +0100

    feat: firebase project config

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,13 @@ interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
     readonly VITE_SHEET_ID: string
     readonly VITE_SHEET_URL: string
+    readonly VITE_FIREBASE_API_KEI: string
+    readonly AUTH_DOMAIN: string
+    readonly PROJECT_ID: string
+    readonly STORAGE_BUCKET: string
+    readonly MESSAGING_SENDER_ID: string
+    readonly APP_ID: string
+    readonly DB: string
   }
   
   interface ImportMeta {
