5e41a96b8d5e7050a20367a99c404f86f9126a3b
Abdelalim <abdelalim.dev@gmail.com>
2023-08-29 18:38:59 +0100

    feat: init API on app start

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css'
 import { AddExpense } from './pages'
 import ExpensesList from './pages/ExpensesList/ExpensesList'
+import { init as apiInit } from './service/api'
+
+apiInit()
 
 function App() {
 
