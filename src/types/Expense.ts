66e3976582fde391221535dfbc95e91961b6f354
Abdelalim <abdelalim.dev@gmail.com>
2023-08-30 08:37:09 +0100

    fix: add key attribute to ExpenseProps

--- src/types/Expense.ts
+++ src/types/Expense.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
 
 export const ZodExpenseSchema = z.object({
+    key: z.string().optional(),
     amount: z.string().nonempty("Set an amount"),
     subcategory: z.string().min(1, "Select a subcategory"),
     wallet: z.string().min(1, "Select a wallet"),
