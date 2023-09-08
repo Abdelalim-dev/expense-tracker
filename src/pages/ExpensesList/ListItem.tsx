ccc55736ee50638a07ff43d8e927ae563e1929a7
Abdelalim <abdelalim.dev@gmail.com>
2023-09-08 09:58:53 +0100

    feat: display wallet as icon in the list

--- src/pages/ExpensesList/ListItem.tsx
+++ src/pages/ExpensesList/ListItem.tsx
@@ -1,19 +1,34 @@
 import { ExpenseProps } from "../../types/Expense";
+import { ComponentProps } from "react";
 
+import bills from "../../assets/icons/icn-bills.svg"
+import doc from "../../assets/icons/icn-doc.svg"
+import groceries from "../../assets/icons/icn-groceries.svg"
+import home from "../../assets/icons/icn-home.svg"
+import kids from "../../assets/icons/icn-kids.svg"
+import us from "../../assets/icons/icn-us.svg"
+const icons: { [key: string]: string } = { bills, doc, groceries, home, kids, us }
+
+
+type IconProps = { name: string } & ComponentProps<"img">
+const Icon = ({ name, ...props }: IconProps) => {
+    return <img src={icons[name]} width={32} alt={`Icon ${name}`} {...props} />
+}
 
 const ListItem = ({ item }: { item: ExpenseProps }) => {
-    return (<div className="bg-white mt-2 py-2 px-4 rounded dark:bg-neutral-800 cursor-pointer">
+    return (<div className="flex bg-white mt-2 py-2 px-4 rounded dark:bg-neutral-800 cursor-pointer">
+        <Icon name={item.wallet.toLowerCase()} className="mr-2 text-xs" />
+        <div>
+            <div className="flex justify-between">
+                <span>{item.subcategory}</span>
 
-        <div className="flex justify-between">
-            <span>{item.subcategory}</span>
+                <span className="font-bold">{item.amount}</span>
+            </div>
 
-            {/* Evaluate amount if not a single value */}
-            <span className="font-bold">{item.amount}</span>
-        </div>
-
-        <div className="flex justify-between text-xs text-gray-500">
-            <span>{item.description}</span>
-            <span>{(new Date(item.date)).toDateString()}</span>
+            <div className="flex justify-between text-xs text-gray-500">
+                <span>{item.description}</span>
+                <span>{(new Date(item.date)).toDateString()}</span>
+            </div>
         </div>
     </div>);
 }
